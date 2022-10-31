bab645fbd7f33de179fd658a1d556a4fcaee187a
Bilalov Danis <hytagyn@gmail.com>
2022-10-31 20:45:39 +0300

    Reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import ru.netology.sqr.SQRService;
 public class Main {
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        int upRange=300;
-        int lowRange=200;
-        int quantity = service.calculate(upRange,lowRange);
+        int upRange = 300;
+        int lowRange = 200;
+        int quantity = service.calculate(upRange, lowRange);
         System.out.println(quantity);
     }
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,43 +1,44 @@
 package ru.netology.sqr;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    public class SQRServiceTest {
-        @Test
-        void rangeEqualToZero() {
-            SQRService service = new SQRService();
+public class SQRServiceTest {
+    @Test
+    void rangeEqualToZero() {
+        SQRService service = new SQRService();
 
-            int upRange = 0;
-            int lowRange = 0;
-            int expected = 0;
+        int upRange = 0;
+        int lowRange = 0;
+        int expected = 0;
 
-            int quantity = service.calculate(upRange, lowRange);
-            assertEquals(expected, quantity);
-        }
-
-        @Test
-        void rangesNegativeValues() {
-            SQRService service = new SQRService();
-
-            int upRange = -800;
-            int lowRange = -700;
-            int expected = 0;
-
-            int quantity = service.calculate(upRange, lowRange);
-            assertEquals(expected, quantity);
-        }
-
-        @Test
-        void rangeNotBeInt() {
-            SQRService service = new SQRService();
-
-            int upRange = (int) Math.pow(10,63);
-            int lowRange = (int) Math.pow(20,64);
-            int expected = 0;
-
-            int quantity = service.calculate(upRange, lowRange);
-            assertEquals(expected, quantity);
-        }
+        int quantity = service.calculate(upRange, lowRange);
+        assertEquals(expected, quantity);
     }
+
+    @Test
+    void rangesNegativeValues() {
+        SQRService service = new SQRService();
+
+        int upRange = -800;
+        int lowRange = -700;
+        int expected = 0;
+
+        int quantity = service.calculate(upRange, lowRange);
+        assertEquals(expected, quantity);
+    }
+
+    @Test
+    void rangeNotBeInt() {
+        SQRService service = new SQRService();
+
+        int upRange = (int) Math.pow(10, 63);
+        int lowRange = (int) Math.pow(20, 64);
+        int expected = 0;
+
+        int quantity = service.calculate(upRange, lowRange);
+        assertEquals(expected, quantity);
+    }
+}
 
